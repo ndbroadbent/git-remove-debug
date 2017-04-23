d4e365d9ff4fe218daae7af8c9ec95b96338edae
Nathan Broadbent <ndbroadbent@users.noreply.github.com>
2017-04-23 21:46:04 +0700

    Actually I sometimes use a condition with debugger, so loosen the regex

--- lib/matchers.rb
+++ lib/matchers.rb
@@ -12,7 +12,7 @@ MATCHERS = {
       },
       {
         name: 'debugger',
-        regex: /^\s*debugger/
+        regex: /debugger/
       },
       {
         name: 'puts',
@@ -29,7 +29,7 @@ MATCHERS = {
     matchers: [
       {
         name: 'debugger',
-        regex: /^\s*debugger/
+        regex: /debugger/
       },
       {
         name: 'console',
